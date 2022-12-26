25f91a87b0745356e33da4a02903d387ce039142
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-12-26 12:45:33 +0300

    inital commit

---
+++ Lets Talk About Everything/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lets Talk About Everything
+//
+//  Created by aytaç bulanık on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
