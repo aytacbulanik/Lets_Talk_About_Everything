26bccd23d4d65a73e11ed64146651adb14989653
Aytaç Bulanık <aytacbulanik@hotmail.com>
2023-01-01 21:17:11 +0300

    create user page created

---
+++ Lets Talk About Everything/Controller/GirisVC.swift
@@ -0,0 +1,35 @@
+//
+//  GirisVC.swift
+//  Lets Talk About Everything
+//
+//  Created by AYTAÇ BULANIK on 1.01.2023.
+//
+
+import UIKit
+
+class GirisVC: UIViewController {
+
+    
+    @IBOutlet var lblMail: UITextField!
+    
+    @IBOutlet var lblSifre: UITextField!
+    
+    @IBOutlet var girisYapOut: UIButton!
+    
+    @IBOutlet var uyeOlOut: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func GirisYapButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func uyeOlButtonPressed(_ sender: Any) {
+    }
+    
+}
---
+++ Lets Talk About Everything/Controller/KayitOlVC.swift
@@ -0,0 +1,39 @@
+//
+//  KayitOlVC.swift
+//  Lets Talk About Everything
+//
+//  Created by AYTAÇ BULANIK on 1.01.2023.
+//
+
+import UIKit
+
+class KayitOlVC: UIViewController {
+
+    @IBOutlet var lblMail: UITextField!
+    
+    @IBOutlet var lblSifre: UITextField!
+    
+    @IBOutlet var lblKullaniciAdi: UITextField!
+    
+    @IBOutlet var uyeOlButtonOut: UIButton!
+    
+    
+    @IBOutlet var vazgectimButtonOut: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func uyeOlButtonPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func vazgectimButtonPressed(_ sender: Any) {
+    }
+    
+    
+
+}
