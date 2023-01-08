d6d4d6e3136981c6465511e54009eeb9aba88516
Aytaç Bulanık <aytacbulanik@hotmail.com>
2023-01-09 01:28:53 +0300

    yorumgetir func wrote

--- Lets Talk About Everything/Model/Yorum.swift
+++ Lets Talk About Everything/Model/Yorum.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Firebase
 
 class Yorum {
     private(set) var kullaniciAdi : String!
@@ -16,5 +17,18 @@ class Yorum {
         self.kullaniciAdi = kullaniciAdi
         self.eklenmeTarihi = eklenmeTarihi
         self.yorumText = yorumText
+    }
+    class func yorumlariGetir(snapshot : QuerySnapshot?) -> [Yorum] {
+        var yorumlar = [Yorum]()
+        guard let snap = snapshot else {return yorumlar}
+        for document in snap.documents {
+            let veri = document.data()
+            let yorumText = veri[YORUM_TEXT] as? String ?? ""
+            let tarih = veri[Eklenme_Tarihi] as? Date ?? Date()
+            let kullaniciAdi = veri[KULLANICIADI] as? String ?? ""
+            let newYorum = Yorum(kullaniciAdi: kullaniciAdi, eklenmeTarihi: tarih, yorumText: yorumText)
+            yorumlar.append(newYorum)
+        }
+        return yorumlar
     }
 }
