a3cbe400c771c1282f3de75e5e11ae041e41db67
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-10-09 23:49:33 +0300

    lets create app again

---
+++ Lets Talk About Everything/WelcomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeVC.swift
+//  Lets Talk About Everything
+//
+//  Created by Aytaç Bulanık on 9.10.2024.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
