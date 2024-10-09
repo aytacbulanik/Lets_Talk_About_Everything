3a397ccecd19a41d22117dd001b283041ac7226c
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-10-10 00:30:53 +0300

    first screeen design completed

--- Lets Talk About Everything/WelcomeVC.swift
+++ Lets Talk About Everything/WelcomeVC.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class WelcomeVC: UIViewController {
 
+    @IBOutlet weak var logInButtonOut: UIButton!
+    @IBOutlet weak var registerButtonOut: UIButton!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    var charIndex = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        logInButtonOut.layer.cornerRadius = 10
+        registerButtonOut.layer.cornerRadius = 10
+       title = "LTAE"
+       let titleText = "Lets Talk About Everything"
+        
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+        
     }
     
 
