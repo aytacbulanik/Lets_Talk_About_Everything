57b03a3b957a5d7445690bb6678c428684b4538b
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-10-10 22:53:43 +0300

    new VC added

---
+++ Lets Talk About Everything/Controller/LogInVC.swift
@@ -0,0 +1,21 @@
+//
+//  LogInVC.swift
+//  Lets Talk About Everything
+//
+//  Created by Aytaç Bulanık on 10.10.2024.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
---
+++ Lets Talk About Everything/Controller/RegisterVC.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterVC.swift
+//  Lets Talk About Everything
+//
+//  Created by Aytaç Bulanık on 10.10.2024.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
