4370c6bf44de554819da8f58d5ead74b180a60bf
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-10-10 23:17:59 +0300

    new user created

--- Lets Talk About Everything/Controller/RegisterVC.swift
+++ Lets Talk About Everything/Controller/RegisterVC.swift
@@ -6,15 +6,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterVC: UIViewController {
 
+    @IBOutlet weak var registerButtonOut: UIButton!
+    @IBOutlet weak var passwordAgainTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        registerButtonOut.layer.cornerRadius = 10
     }
     
-
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        Auth.auth().createUser(withEmail: "ayt@gmail.com", password: "112233") { AuthDataResult, error in
+            if error != nil {
+                if let error {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
    
 }
